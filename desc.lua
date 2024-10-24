179b39fd8a4452e9cbd247202014c0a832ed3f5a
pocomane <none@mail.com>
2024-10-24 23:54:20 +0200

    flat file organization

--- desc.lua
+++ desc.lua
@@ -1,11 +1,11 @@
 
 {
   output = {
-    { 'a5.html', 'example/hello_world.md' },
-    { 'a5-twocol.html', 'example/hello_world.md', section_decoration=true },
-    { 'a4.html', 'example/hello_world.md', section_decoration=true, content='<div style="page-break-after: always;" /><img style="width: 100%;" src="../example/star.svg" />' },
-    { 'a5.html', 'example/hello_world.md', add_front_page=true, insert_image=true, },
-    { 'a5-twocol.html', 'example/hello_world.md', add_front_page=true, insert_image=true, },
+    { 'a5.html', './hello_world.md' },
+    { 'a5-twocol.html', './hello_world.md', section_decoration=true },
+    { 'a4.html', './hello_world.md', section_decoration=true, content='<div style="page-break-after: always;" /><img style="width: 100%;" src="../image.svg" />' },
+    { 'a5.html', './hello_world.md', add_front_page=true, insert_image=true, },
+    { 'a5-twocol.html', './hello_world.md', add_front_page=true, insert_image=true, },
   },
   path = {
    build = "./build",
@@ -13,13 +13,13 @@
   title = {
     text = "TheTitle",
     author = "by Author",
-    image = "../example/star.svg",
+    image = "../image.svg",
   },
   section_pre = {
      ['## Subsection'] = '</div><div>',
   },
   section_image = {
-     ['# SECTION'] = '../example/star.svg',
+     ['# SECTION'] = '../image.svg',
   },
   snippet = {
     font = "../gentium.ttf",
